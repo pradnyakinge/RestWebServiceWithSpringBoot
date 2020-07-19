5d1e810d52b606a546864519aac2bf69bb039b7c
pradnyakinge <pradnya.kolte1990@gmail.com>
2020-07-19 15:49:41 -0400

    update response field messages for swagger file

--- RestWebService/src/main/java/com/myRestService/bean/UserBean.java
+++ RestWebService/src/main/java/com/myRestService/bean/UserBean.java
@@ -9,8 +9,14 @@ import javax.persistence.Table;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
 @Table(name = "user")
+@ApiModel(description = "All details about the user.")
 public class UserBean {
 	
 	@Id
@@ -18,9 +24,12 @@ public class UserBean {
 	private int id;
 	
 	@Size(min = 2, max = 6, message = "name should be atleast 2 char long")
+	@ApiModelProperty(notes = "name should be atleast 2 char long")
 	private String name;
 	
+	//@JsonIgnore
 	@Past(message = "future date shoudn't be accepted.")
+	@ApiModelProperty(notes = "Birthdate should be in the past")
 	private Date birthdate;
 	public int getId() {
 		return id;
