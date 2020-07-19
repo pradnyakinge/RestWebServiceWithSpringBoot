78993fd7a8157df33f6526d9da3b96fe16a1e038
pradnyakinge <pradnya.kolte1990@gmail.com>
2020-07-19 15:22:44 -0400

    configuring swagger to application

---
+++ RestWebService/src/main/java/com/myRestService/swagger/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.myRestService.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	//define docket bean
+	@Bean
+	public Docket api()
+	{
+		//configure swagger 2
+		//all path
+		//all apis
+		
+		
+		return new Docket(DocumentationType.SWAGGER_2);
+	}
+
+}
