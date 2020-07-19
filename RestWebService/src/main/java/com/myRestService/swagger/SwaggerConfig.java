3e511a9da0f3a2ebaafbf160f75752fa7ab4a657
pradnyakinge <pradnya.kolte1990@gmail.com>
2020-07-19 15:33:34 -0400

    modified swagger configuration

--- RestWebService/src/main/java/com/myRestService/swagger/SwaggerConfig.java
+++ RestWebService/src/main/java/com/myRestService/swagger/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package com.myRestService.swagger;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,6 +16,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	public static final Contact DEFAULT_CONTACT = new Contact("Pradnya Kinge", "http://www.restDemo.com", "pradnya.kolte1990@gmail.com");
+	
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awsome API Title", "Awsome Api Documentation", "1.0", "urn:tos",
+	          DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json","application/xml"));
+    
+	
 	//define docket bean
 	@Bean
 	public Docket api()
@@ -17,9 +31,9 @@ public class SwaggerConfig {
 		//configure swagger 2
 		//all path
 		//all apis
-		
-		
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO)
+                									  .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                									  .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 
 }
